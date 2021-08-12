69c641714047b185ec2c5740b70c0322aa01893a
rsnytl <rsnytl@gmail.com>
2021-08-12 09:48:19 +0530

    Completed lazyloading of images

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -58,9 +58,7 @@ class StickyHeader {
       ) {
         let matchingLink = el.getAttribute("data-matching-link");
         document
-          .querySelectorAll(`.primary-nav a:not(${matchingLink})`)
-          .forEach(el => el.classList.remove("is-current-link"));
-
+        .querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(el => el.classList.remove("is-current-link"));        
         document.querySelector(matchingLink).classList.add("is-current-link");
       }
     }
