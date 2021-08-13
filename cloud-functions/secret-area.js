c01e0cee722aaf41ced17e515546085e0b0df8c4
rsnytl <rsnytl@gmail.com>
2021-08-13 08:45:48 +0530

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,7 +1,28 @@
 exports.handler = function(event, context, callback){
+    const secretContent =`
+    <h3>Welcome to the Secret Area</h3>
+    <p>Here we can tell you the sky is <strong>blue</strong>, and two plus two is four.</p>
+    `
 
-    callback(null,{
-        statusCode:200,
-        body:"welcome to the super secret area"
-    })
+    let body;
+
+    if(event.body){
+        body=JSON.parse(event.body);
+    }else{
+        body={};
+    }
+
+    if(body.password=='javascript'){
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else{
+        callback(null, {
+            statusCode: 401,
+            
+        })
+    }
+
+
 }
